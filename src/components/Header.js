05466aa6722e86e96892057b2b9f32f7fd825f20
artorrias <sam.henson1119@outlook.com>
2023-09-24 21:33:10 -0600

    added tab functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,31 @@
 //require navigation
 import React, { useState } from 'react';
 import Navigation from './Navigation';
+import { act } from 'react-dom/test-utils';
 
 function Header() {
-    let [category, setCategory] = useState('About');
 
-    const clickHandler = (e) => {
-        e.preventDefault();
+    const [activeTab, setTab] = useState("About");
 
-        const { target } = e;
-        const navType = target.type;
+    const handleAbout = () => {
+        setTab("About");
+    };
 
-        if (navType != category) {
-            setCategory(navType);
-        }
-
+    const handleResume = () => {
+        setTab("Resume");
     };
 
     return (
-                <div>
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <p className="navbar-brand">Sam Henson</p>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div className="navbar-nav">
-                                <p className="nav-item nav-link active" onClick={clickHandler}>About</p>
-                                <p className="nav-item nav-link" onClick={clickHandler}>Resume</p>
-                            </div>
-                        </div>
-                    </nav>
-                    <Navigation 
-                        section={category}>
-                    </Navigation>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <h1 className='navbar-brand'>Sam Henson</h1>
+                <div className="navbar-nav">
+                    <p className={activeTab==="About" ? "nav-item nav-link active" : "nav-item nav-link"} onClick={handleAbout}>About Me</p>
+                    <p className={activeTab==="Resume" ? "nav-item nav-link active" : "nav-item nav-link"} onClick={handleResume}>Resume</p>
                 </div>
-    );
+            </nav>
+            {activeTab === "About" ? <Navigation section="About"/> : <Navigation section="Resume"/>} 
+        </div>
+    )
 }
-
 export default Header;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,26 +4,26 @@ import Footer from './Footer';
 
 function Navigation(props) {
     //use state set which page we're on
-    const [nav, setNav] = useState(props.section);
+    let variable = props.section;
 
 
     //function that checks the page and returns code depending on page
-    if (nav === 'About') {
-        return (
+    return (
+        <div className="outlet"> 
+        {variable==="About" ?
+            (
+                <div>
+                    <p>about</p>
+                </div>
+            )
+        : (
             <div>
-                <p>Sam Henson</p>
-                <p>insert image</p>
+                <p>Resume</p>
             </div>
-        );
-    }
-    else if (nav==='Resume') {
-        return (
-            <div>
-                <p>enter resume here</p>
-            </div>
-        );
-    }
-}
+        )}
+        </div>
+    );
+};
 
 export default Navigation;
 
