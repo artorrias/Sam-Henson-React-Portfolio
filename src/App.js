f513da99798f57198c733bd60c3c66fbb8968b01
artorrias <sam.henson1119@outlook.com>
2023-09-24 18:46:48 -0600

    added base files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BucketList from './components/Portfolio'; //change if needed
+
+function App() {
+    return (
+      <div className="Portfolio-app">
+        <Portfolio />
+      </div>
+    );
+  }
+  
+  export default App;
